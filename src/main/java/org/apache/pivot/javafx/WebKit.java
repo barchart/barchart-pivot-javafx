a3c36d7dd7075d5884053169331620131600e091
Andrei.Pozolotin <Andrei.Pozolotin@gmail.com>
2013-02-26 11:57:01 -0600

    project template

---
+++ src/main/java/org/apache/pivot/javafx/WebKit.java
@@ -0,0 +1,10 @@
+package org.apache.pivot.javafx;
+
+import javafx.scene.web.WebView;
+
+public class WebKit {
+
+	/** Verify dependency. */
+	private WebView webView;
+
+}
---
+++ src/test/java/org/apache/pivot/javafx/WebKitTest.java
@@ -0,0 +1,17 @@
+package org.apache.pivot.javafx;
+
+
+import org.junit.After;
+import org.junit.Before;
+
+public class WebKitTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+}
